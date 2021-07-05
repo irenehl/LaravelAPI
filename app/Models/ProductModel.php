33865d1906267c8179e10def6ebe7e51d20a7418
irenehl <26irenelopez@gmail.com>
2021-07-05 14:33:35 -0600

    ✨ Creating controllers

--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class ProductModel extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'sku',
+        'name',
+        'stock',
+        'price',
+        'description'
+    ];
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class UserModel extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'username',
+        'email',
+        'password',
+        'phone',
+        'dob'
+    ];
 }
